6352619f47a72b2aab5bac09fac7996af0e1f0c1
Hibam Iru Dionisio Alor <hibamiru@gmail.com>
2015-06-09 14:08:23 -0500

    Prueba de no escritura en config

--- centro-seguimiento/config.php
+++ centro-seguimiento/config.php
@@ -6,4 +6,4 @@
 ini_set('display_errors', true);
 error_reporting(E_ALL);
 
-define('cs_url', '', true); //Url de la instalación sin la barra final. Ej. http://localhost/dev.musinetwork.com/centro-seguimiento
+define('cs_url', 'http://localhost/dev.musinetwork.com/centro-seguimiento', true); //Url de la instalación sin la barra final. Ej. http://localhost/dev.musinetwork.com/centro-seguimiento
