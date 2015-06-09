2f724dc651164105f9e1acbec36a69e79b480d0b
Hibam Iru Dionisio Alor <hibamiru@gmail.com>
2015-06-09 14:00:42 -0500

    Se agregó constante de url de instalacion

--- centro-seguimiento/config.php
+++ centro-seguimiento/config.php
@@ -5,3 +5,5 @@
 
 ini_set('display_errors', true);
 error_reporting(E_ALL);
+
+define('cs_url', '', true); //Url de la instalación sin la barra final. Ej. http://localhost/dev.musinetwork.com/centro-seguimiento
