254aa4fb84543dc56f9e87c6d2f33c1689eb450b
Hibam Iru Dionisio Alor <hibamiru@gmail.com>
2015-06-09 13:46:00 -0500

    Se quitaron archivos obsoletos y de pruebas. Se agrego logout a nuevo fw

--- centro-seguimiento/includes/footer.php
+++ centro-seguimiento/includes/footer.php
@@ -4,7 +4,7 @@
 	<?php if (logged_in()) {?>
 		<div id="sesion-usuario">
 			<span>Hola, <strong><?php $nombre_operador = $_SESSION['nombre'];  echo $nombre_operador; ?></strong> | </span>
-			<span class="sesion-cerrar"><a href="logout.php"> Cerrar sesion</a></span>
+			<span class="sesion-cerrar"><a href="logout"> Cerrar sesion</a></span>
 		</div><!-- fin #sesion-usuario -->
 	<?php } ?>	
 	</div><!-- fin #footer-content -->
