81e71266c663dd07c880d6fbc4c7051bea1635fe
Hibam Iru Dionisio Alor <hibamiru@gmail.com>
2015-06-14 07:36:40 -0500

    Config se subió sin parametros

--- config.php
+++ config.php
@@ -8,9 +8,9 @@ define("DEBUG", TRUE);
 
 // Constantes de BD
 define("DB_SERVER", "localhost");
-define("DB_USER", "admin");
-define("DB_PASS", "acceso01");
-define("DB_NAME", "devcentroseguimiento");
+define("DB_USER", "");
+define("DB_PASS", "");
+define("DB_NAME", "");
 
 // URL de instalación
 define('cs_url', 'http://localhost/dev.musinetwork.com', true); //Url de la instalación sin la barra final. Ej: http://localhost/dev.musinetwork.com/centro-seguimiento
