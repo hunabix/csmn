1f0eb0b0be4d73fe3b5403cd2ceb5745dc627758
Hibam Iru Dionisio Alor <hibamiru@gmail.com>
2015-06-18 00:19:42 -0500

    Se agregó procesar.php

---
+++ controllers/procesar.php
@@ -0,0 +1,19 @@
+<?php
+/*
+* Controlador que recibe peticiones
+*/
+
+confirm_logged_in(); //revisa si el operador ha ingresado
+
+if (isset($_POST['formulario'])) {
+
+	$nombreFormulario = $_POST['formulario'];
+	
+	echo '<h2>Llego por el formulario '. $nombreFormulario . '</h2>';
+
+	// Impresión de parametros para testing
+	echo json_encode($_POST);
+	
+}
+//view('home', compact('casos', 'nombre_vista', 'saludo'));
+
