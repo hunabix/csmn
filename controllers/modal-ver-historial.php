8d1adac20b72971f0a3120ed57fe85bc4e3dabcd
Hibam Iru Dionisio Alor <hibamiru@gmail.com>
2015-06-20 01:09:18 -0500

    Se metió el historial en un array junto con lead-id y tipo-accion y con guines bajos

--- controllers/modal-ver-historial.php
+++ controllers/modal-ver-historial.php
@@ -37,8 +37,8 @@ if ($query->execute(array(
 	
 	
 	//Returning lead ID
-	$return['lead_id']		= $data['lead_id'];
-	$return['tipo_accion']	= $data['tipo_accion'];
+	$return['lead_id']		= $data['lead-id'];
+	$return['tipo_accion']	= $data['tipo-accion'];
 	$return['historial']	= $result;
 	//print_array($return);
 	echo json_encode($return, JSON_UNESCAPED_UNICODE);		
