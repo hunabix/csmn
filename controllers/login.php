76801d801ce505b2afb24e7c598d879e83e0e264
Hibam Iru Dionisio Alor <hibamiru@gmail.com>
2015-06-20 11:24:49 -0500

    Se agregó consulta para controllador login

--- controllers/login.php
+++ controllers/login.php
@@ -1,8 +1,51 @@
 <?php
 /*
-* Controlador del Home / Página principal
+* Controlador del Login
 */
+
 		
+//COMIENZA A PROCESAR EL DOCUMENTO
+if (isset($_POST['ingreso'])) { // el formulario ha sido enviado
+
+	// recibo la información		
+	
+	$usuario = $_POST['usuario'];
+	$clave = $_POST['clave'];
+	
+	//reviso en la base de datos si el usuario existe
+	$consulta =  "SELECT *
+					FROM operador_mn
+					WHERE usuario = '$usuario'
+					AND clave = '$clave'
+					LIMIT 1";
+	global $connection;
+	$operador_set = mysql_query($consulta, $connection);
+	confirm_query($operador_set);
+	
+	if (mysql_num_rows($operador_set) ==1) {
+		// autentificado!!!!!!!!!!
+		$operador = mysql_fetch_array($operador_set);
+		$_SESSION['user_id'] = $operador['ID'];
+		$_SESSION['username'] = $operador['usuario'];
+		$_SESSION['nombre'] = $operador['nombre']; 
+		//redirijo por javascript a index ?>				
+		<script type="text/javascript">			
+			window.location="<?php echo cs_url; ?>/home";			
+		</script>
+	<?php }
+	else {
+		//$mensaje = "Nombre de usuario o contraseña incorrecto";
+		header('Location: ' . cs_url . '/login');
+		exit;
+	}
+	
+} else { // el formulario no ha sido enviado
+		header('Location: ' . cs_url . '/login');
+		exit;
+}
+			
+ 	
+
 //Llamando una vista
 view('login');
 
