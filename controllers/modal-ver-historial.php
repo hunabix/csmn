854390349ae6fefd72840fc8d77ee6c9f590f954
Hibam Iru Dionisio Alor <hibamiru@gmail.com>
2015-06-19 23:48:50 -0500

    Se agregó consulta de historial y se envían solo los datos necesarios.

--- controllers/modal-ver-historial.php
+++ controllers/modal-ver-historial.php
@@ -10,11 +10,36 @@
 // Get historial by lead id
 $con = db_con();
 $query = $con->prepare('SELECT * FROM interaccion_cs WHERE id_interesado = :id_interesado ORDER BY fecha DESC');
-$query->execute(array('id_interesado' => $data['lead-id']));
-$historial = $query->fetchAll();
-$query->closeCursor();
-
-//Returning lead ID
-$return['lead-id'] = $data['lead-id'];
-$return['tipo-accion'] = $data['tipo-accion'];
-echo json_encode($historial, JSON_UNESCAPED_UNICODE);
+if ($query->execute(array(
+		'id_interesado' => $data['lead-id']
+	))) {
+		
+	$historial = $query->fetchAll();
+	$query->closeCursor();
+	
+	$historial;
+	
+	foreach ($historial as $key => $value) {
+	
+		// Adding last interaction info
+		$value['alerta']		= $value['alerta'];
+		$value['fecha']			= fecha_en_array($value['recordatorio']);
+		$value['hora']			= fecha_en_array($value['recordatorio']);
+		$value['mensaje_int']	= $value['mensaje_int'];
+		$value['mensaje_op']	= $value['mensaje_op'];
+		$value['observaciones']	= $value['observaciones'];
+		$value['tipo']			= $value['tipo'];
+		
+		// Saving info
+		$result[$key] = $value;
+		//print_array($casos);
+	}
+	
+	
+	//Returning lead ID
+	$return['lead-id']		= $data['lead-id'];
+	$return['tipo-accion']	= $data['tipo-accion'];
+	$return['historial']	= $result;
+	echo json_encode($result, JSON_UNESCAPED_UNICODE);		
+		
+}
