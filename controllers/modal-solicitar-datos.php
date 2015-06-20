540d9e3c0757ccf16c0c0bea37a595bdf850a68e
Hibam Iru Dionisio Alor <hibamiru@gmail.com>
2015-06-20 01:17:43 -0500

    Se agregó consulta a modal-solicitar-datos

--- controllers/modal-solicitar-datos.php
+++ controllers/modal-solicitar-datos.php
@@ -10,11 +10,17 @@
 // Get data by lead id
 $con = db_con();
 $query = $con->prepare('SELECT * FROM interesado_cs WHERE ID = :ID');
-$query->execute(array('ID' => $data['lead-id']));
-$lead_info = $query->fetch();
-$query->closeCursor();
-
-//Returning lead ID
-$return['lead-id'] = $data['lead-id'];
-$return['tipo-accion'] = $data['tipo-accion'];
-echo json_encode($lead_info, JSON_UNESCAPED_UNICODE);
+if ($query->execute(array('ID' => $data['lead-id']))) {
+	
+	$lead_info = $query->fetch();
+	$query->closeCursor();
+	
+	//Returning lead ID
+	$return['lead_id'] = $data['lead-id'];
+	$return['tipo_accion'] = $data['tipo-accion'];
+	$return['lead_info'] = $lead_info;
+	
+	echo print_array($return);
+	echo json_encode($return, JSON_UNESCAPED_UNICODE);
+	
+}
