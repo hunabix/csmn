7c3c8ed3c5e9979ca58c6f8225f15b2ca7d2b581
Hibam Iru Dionisio Alor <hibamiru@gmail.com>
2015-06-19 22:34:15 -0500

    Se agregó consulta cambiar prioridad.

--- controllers/modal-cambiar-prioridad.php
+++ controllers/modal-cambiar-prioridad.php
@@ -6,3 +6,23 @@
 //echo $data['tipo-accion'];
 
 //echo print_array($data);
+
+
+$lead = array();
+$con = db_con();
+
+// Changes priority
+$query = $con->prepare('UPDATE interesado_cs SET prioridad = :prioridad WHERE ID = :ID');
+if ($query->execute(array(
+		'prioridad' => $data['prioridad'],
+		'ID' => $data['lead-id'],
+	))) {
+
+	//Returning lead ID
+	$return['lead_id'] = $data['lead-id'];
+	$return['tipo_accion'] = $data['tipo-accion'];
+	$return['mensaje'] = 'Se cambió la prioridad a ' . $data['prioridad'];
+	echo json_encode($return, JSON_UNESCAPED_UNICODE);
+}
+
+$query->closeCursor();
