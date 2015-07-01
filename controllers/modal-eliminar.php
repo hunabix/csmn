ecab0af08fa67c861e5f1db0d6bff9cfa9da527e
Hibam Iru Dionisio Alor <hibamiru@gmail.com>
2015-06-30 23:45:49 -0500

    Eliminar ya funciona en individual y multimodal

--- controllers/modal-eliminar.php
+++ controllers/modal-eliminar.php
@@ -7,18 +7,50 @@
 
 //echo print_array($data);
 
-// Deleting lead
+//Initialize lead_ids
+$lead_ids = array();
+
+// Process lead ids
+if ($data['formulario'] == 'leads-form') {
+	$lead_ids[0] = $data['lead-id'];
+}
+
+if ($data['formulario'] == 'mag-form') {
+	$lead_ids = extract_checkbox_ids($data);
+}
+
+// Prepare connection
 $lead = array();
 $con = db_con();
 $query = $con->prepare('DELETE FROM interesado_cs WHERE ID = :id');
 
-if ( $query->execute(array('id' => $data['lead-id'])) ) {
+//file_put_contents("tracker.log",print_r($lead_ids,true));
+
+foreach ($lead_ids as $lead_id) {
+	
+	if ( $query->execute(array('id' => $lead_id)) ) {
+
+		//Returning info for ajax request
+		if ($data['formulario'] == 'leads-form') {	
+
+			//Returning lead ID
+			$return['lead_id'] = $lead_id;
+			$return['tipo_accion'] = $data['tipo-accion'];
+			$return['mensaje'] = 'Se eliminó el registro';
+			echo json_encode($return, JSON_UNESCAPED_UNICODE);
+			
+		}
+	
+	}
+
+}
+
+if ($data['formulario'] == 'mag-form') {
 
 	//Returning lead ID
-	$return['lead_id'] = $data['lead-id'];
-	$return['tipo_accion'] = $data['tipo-accion'];
-	$return['mensaje'] = 'Se eliminó el registro';
+	$return = TRUE;
 	echo json_encode($return, JSON_UNESCAPED_UNICODE);
 
 }
+
 $query->closeCursor();
